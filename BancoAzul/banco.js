ab598102fdf8ae9526bca0bb6cfe0c4ba95a4f9d
OtavioGandhi <164433943+OtavioGandh@users.noreply.github.com>
2024-10-30 21:56:38 -0300

    Add files via upload

---
+++ BancoAzul/banco.js
@@ -0,0 +1,62 @@
+const limiteSaques = 3;
+
+let saldo = 0;
+let extrato = "";
+let limite = 500;
+let numeroSaques = 0;
+
+while (true) {
+  menu = prompt(`
+        ============================
+        [d] - Depósito
+        [s] - Saque
+        [e] - Extrato
+        [q] - Sair
+        ============================
+        `);
+
+  if (menu == "d") {
+    valor = parseFloat(prompt("Informe o valor do depósito: R$"));
+    if (valor > 0) {
+      saldo += valor;
+      
+      extrato += `Depósito de R$${valor} \n`;
+      alert(`Depósito de R$${valor} realizado.`);
+    } else {
+      alert("Operação falhou! O valor informado é inválido!");
+    }
+  } else if (menu == "s") {
+        valor = parseFloat(prompt("Informe o valor do saque: R$"));
+        excedeuSaldo = valor > saldo;
+        excedeuLimite = valor > limite;
+        excedeuSaque = numeroSaques >= limiteSaques;
+    if (excedeuSaldo) {
+      alert("Operação falhou! Você não tem saldo suficiente!");
+    } else if (excedeuLimite) {
+      alert("Operação falhou! O valor do saque excede o limite!");
+    } else if (excedeuSaque) {
+      alert("Operação falhou! Número máximo de saques excedido!");
+    } else if (valor > 0) {
+      saldo -= valor;
+      extrato += `Saque de R$${valor} \n`;
+      alert(`Saque de R$${valor} relizado`);
+      numeroSaques++; 
+    } else {
+      alert("Operação falhou! O valor informado é inválido!");
+    }
+  } else if (menu == "e") {
+    mensagem = extrato == "" ? "Não foram realizadas movimentações." : extrato;
+  
+    alert(`
+    =======E X T R A T O =============
+    ${mensagem}
+    Saldo: R$ ${saldo.toFixed(2)}
+    ==================================
+    `);
+  } else if (menu == "q") {
+    alert("Obrigado pela preferência !!!");
+    break;
+  } else {
+    alert("Opção Inválida, por favor selecione novamente a operação desejada.");
+  }
+}
